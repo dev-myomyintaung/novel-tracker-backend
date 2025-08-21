a25e79d3f9ab26bff78d7b3c218c673b2cc1b36a
Myo Myint Aung <myomyintaung064@gmail.com>
2025-08-21 20:01:04 +0800

    Refactor code for consistency and readability; update novel creation and update logic to include rating and genre details.

--- src/middlewares/authenticate-token.ts
+++ src/middlewares/authenticate-token.ts
@@ -1,29 +1,21 @@
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import {JwtUserPayload} from "../types/express";
-import {prisma} from "../models/prisma-client";
+import { JwtUserPayload } from "../types/express";
 
-const JWT_SECRET = process.env.JWT_SECRET || 'dev_secret'
+const JWT_SECRET = process.env.JWT_SECRET || "dev_secret";
 
-const authenticateToken = (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    const token = req.cookies.token;
-    console.log("token", token);
-    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.cookies.token;
+  console.log("token", JSON.stringify(req.cookies));
+  if (!token) return res.status(401).json({ error: "Unauthorized" });
 
-    try {
-        req.user = jwt.verify(
-            token,
-            JWT_SECRET as string
-        ) as JwtUserPayload;
+  try {
+    req.user = jwt.verify(token, JWT_SECRET as string) as JwtUserPayload;
 
-        next();
-    } catch (err) {
-        return res.status(403).json({ error: 'Invalid token' });
-    }
+    next();
+  } catch (err) {
+    return res.status(403).json({ error: "Invalid token" });
+  }
 };
 
 export default authenticateToken;
